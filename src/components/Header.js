111093a151dfc12404fd27bcd5263171f260c70c
rajni62 <rajni@fkointech.com>
2024-08-27 13:50:32 +0530

    Header fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
     return (<>
         <div className="bg-background">
             <div className="max-w-7xl mx-auto relative z-10">
-                <div className="flex justify-between items-center">
+                <div className="flex justify-between items-center relative z-10">
                     <div className="text-white flex gap-5">
                         <button className="border border-white  px-4 py-[6px] rounded-lg gradient-bg">Sign In</button>
                         <button className="border border-white px-4 py-[6px] rounded-lg hover:bg-gradient-to-t from-[#F5BC46] to-[#ee7b31]">Sign Up</button>
@@ -14,7 +14,7 @@ export default function Header() {
                         <div className="flex gap-6 text-white">
                             <Link href="/" className="text-xl font-medium">Contact</Link>
                             <Link href="/" className="text-xl font-medium">Services</Link>
-                            <Link href="/" className="text-xl font-medium">Profile</Link>
+                            <Link href="/profile" className="text-xl font-medium">Profile</Link>
                             <Link href="/" className="text-xl font-medium">Hiring</Link>
                             <Link href="/" className="text-xl font-medium">Discounts</Link>
                             <Link href="/" className="text-xl font-medium">Deals</Link>

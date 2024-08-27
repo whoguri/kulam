317d2ec0b59826998b3f2ba1f86d6a6b251b843e
rajni62 <rajni@fkointech.com>
2024-08-27 18:15:29 +0530

    Added meta titles

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,11 @@
 import Hero from "@/components/Hero";
 import Header from "@/components/Header"
 import HomeBanner from "@/components/HomeBanner"
+import { APP_NAME } from "@/constents/constArray";
+
+export const metadata = {
+  title: ` ${APP_NAME}`, description: "",
+};
 
 export default function Home() {
   return (<>

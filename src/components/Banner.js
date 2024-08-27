3b2830ddf9a6bfe76f5a23e8b1bb8f0d8df7a510
Kulwinder Singh <kulwinder@fkointech.com>
2024-08-27 17:18:49 +0530

    banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -17,12 +17,12 @@ export default function Banner({ title }) {
             <h2 className="md:text-[120px] text-5xl leading-none uppercase md:py-3 py-[10px] font-bold text-text text-right">all in one</h2>
             {isHome && <h3 className="md:text-2xl text-base text-right text-white">Together We Can Beat The Price</h3>}
 
-            {isHome && <div className="md:flex gap-6 items-center justify-center md:pt-16 pt-10 md:pb-12 pb-8 relative z-20">
-                <button className="px-4 py-[6px] md:text-lg text-sm rounded-lg gradient-bg text-white">Advertiser 5</button>
-                <button className="px-4 py-[6px] md:text-lg text-sm rounded-lg gradient-bg text-white">Advertiser 4</button>
-                <button className="px-4 py-[6px] md:text-lg text-sm rounded-lg gradient-bg text-white">Advertiser 3</button>
-                <button className="px-4 py-[6px] md:text-lg text-sm rounded-lg gradient-bg text-white">Advertiser 2</button>
-                <button className="px-4 py-[6px] md:text-lg text-sm rounded-lg gradient-bg text-white">Advertiser 1</button>
+            {isHome && <div className="grid md:grid-cols-5 grid-cols-3 md:w-[70%] w-full mx-auto md:gap-6 gap-3 items-center justify-center md:pt-16 pt-10 md:pb-12 pb-8 relative z-20">
+                <button className="md:px-4 px-3 py-[6px] md:text-lg text-sm rounded-lg gradient-bg text-white">Advertiser 5</button>
+                <button className="md:px-4 px-3 py-[6px] md:text-lg text-sm rounded-lg gradient-bg text-white">Advertiser 4</button>
+                <button className="md:px-4 px-3 py-[6px] md:text-lg text-sm rounded-lg gradient-bg text-white">Advertiser 3</button>
+                <button className="md:px-4 px-3 py-[6px] md:text-lg text-sm rounded-lg gradient-bg text-white">Advertiser 2</button>
+                <button className="md:px-4 px-3 py-[6px] md:text-lg text-sm rounded-lg gradient-bg text-white">Advertiser 1</button>
             </div>}
             {isHome && <Image src="/images/title.svg" width={50} height={50} className="w-[400px] mx-auto absolute bottom-0 right-0 z-10 left-0" />}
         </div>

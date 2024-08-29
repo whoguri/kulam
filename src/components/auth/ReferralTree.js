4b711aadd7ae48176b015ad2b759c30b4d7b8c4e
Parminder Kaur <parminder@fkointech.com>
2024-08-29 13:39:09 +0530

    ..

--- src/components/auth/ReferralTree.js
+++ src/components/auth/ReferralTree.js
@@ -1,8 +1,8 @@
 function ReferralNode({ node, level, index, isLast }) {
     return (<div className={`border-x px-6 py-6 ${isLast && "rounded-b-md"} ${index === 0 ? "border-t rounded-t-md" : ""} ${level !== 2 ? "border-b" : "border rounded-md"}`}>
-        <h3 className={`capitalize ${level === 1 ? "pb-4" : ""}`}>{node.name} {node.referrals.length > 0 ? <>({node.referrals.length})</> : ""}</h3>
+        <h3 className={`capitalize ${level === 1 ? "" : ""}`}>{node.name} {node.referrals.length > 0 ? <>({node.referrals.length})</> : ""}</h3>
         {node.referrals.length > 0 && (
-            <div className={`grid ${level === 1 ? "md:grid-cols-3 grid-cols-1 text-center" : ""}`}>
+            <div className={`grid ${level === 1 ? "md:grid-cols-3 grid-cols-1 text-center pt-4" : ""}`}>
                 {node.referrals.map(ref => (
                     <ReferralNode key={ref.id} node={ref} level={level + 1} />
                 ))}

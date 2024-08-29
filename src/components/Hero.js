9f763582286f256a24f6d23f54b86fb2d51d80bc
Kulwinder Singh <kulwinder@fkointech.com>
2024-08-29 11:15:13 +0530

    home

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -18,10 +18,10 @@ export default function Hero() {
             <div className="2xl:max-w-7xl xl:max-w-6xl max-w-[90%] mx-auto py-10">
                 <div className="md:flex gap-10 w-full">
                     <div className="md:w-[15%] w-full flex md:flex-col flex-row md:gap-10 gap-3 justify-between  md:mb-0 mb-5">
-                        <div className="bg-white w-full md:py-10 py-5 md:px-0 px-3 text-center md:text-xl text-sm font-medium rounded-xl">Deal 1</div>
-                        <div className="bg-white w-full md:py-10 py-5 md:px-0 px-3 text-center md:text-xl text-sm font-medium rounded-xl">Deal 2</div>
-                        <div className="bg-white w-full md:py-10 py-5 md:px-0 px-3 text-center md:text-xl text-sm font-medium rounded-xl">Deal 3</div>
-                        <div className="bg-white w-full md:py-10 py-5 md:px-0 px-3 text-center md:text-xl text-sm font-medium rounded-xl">Deal 4</div>
+                        <div className="bg-white w-full 2xl:py-9 xl:py-8 py-5 2xl:px-0 xl:px-0 px-3 text-center md:text-xl text-sm font-medium rounded-xl">Deal 1</div>
+                        <div className="bg-white w-full 2xl:py-9 xl:py-8 py-5 2xl:px-0 xl:px-0 px-3 text-center md:text-xl text-sm font-medium rounded-xl">Deal 2</div>
+                        <div className="bg-white w-full 2xl:py-9 xl:py-8 py-5 2xl:px-0 xl:px-0 px-3 text-center md:text-xl text-sm font-medium rounded-xl">Deal 3</div>
+                        <div className="bg-white w-full 2xl:py-9 xl:py-8 py-5 2xl:px-0 xl:px-0 px-3 text-center md:text-xl text-sm font-medium rounded-xl">Deal 4</div>
                     </div>
                     <div className="md:w-[70%] w-full">
                         <Slider {...settings}>
@@ -35,7 +35,7 @@ export default function Hero() {
                                     <button className="border border-white text-white px-4 py-[6px] md:mt-6 mt-4 md:text-lg text-sm rounded-lg gradient-bg">Learn More</button>
                                 </div>
                             </div>
-                            <div className="md:!flex justify-between gap-5 md:py-8 py-4 md:px-8 px-4 bg-white rounded-xl">
+                            <div className="md:!flex justify-between gap-5 md:p-8 p-4 bg-white rounded-xl">
                                 <div className="md:w-[30%] w-full">
                                     <Image src="/images/dummy.jfif" alt="dummy" width={225} height={224} className="rounded-xl md:w-[225px] w-full" />
                                 </div>
@@ -45,7 +45,7 @@ export default function Hero() {
                                     <button className="border border-white text-white px-4 py-[6px] md:mt-6 mt-4 md:text-lg text-sm rounded-lg gradient-bg">Learn More</button>
                                 </div>
                             </div>
-                            <div className="md:!flex justify-between gap-5 md:py-8 py-4 md:px-8 px-4 bg-white rounded-xl">
+                            <div className="md:!flex justify-between gap-5 md:p-8 p-4 bg-white rounded-xl">
                                 <div className="md:w-[30%] w-full">
                                     <Image src="/images/dummy.jfif" alt="dummy" width={225} height={224} className="rounded-xl md:w-[225px] w-full" />
                                 </div>
@@ -58,10 +58,10 @@ export default function Hero() {
                         </Slider>
                     </div>
                     <div className="md:w-[15%] w-full flex md:flex-col flex-row md:gap-10 gap-3 justify-between md:mt-0 mt-14">
-                        <div className="bg-white w-full md:py-10 py-5 md:px-0 px-3 text-center md:text-xl text-sm font-medium rounded-xl">Text</div>
-                        <div className="bg-white w-full md:py-10 py-5 md:px-0 px-3 text-center md:text-xl text-sm font-medium rounded-xl">Text</div>
-                        <div className="bg-white w-full md:py-10 py-5 md:px-0 px-3 text-center md:text-xl text-sm font-medium rounded-xl">Text</div>
-                        <div className="bg-white w-full md:py-10 py-5 md:px-0 px-3 text-center md:text-xl text-sm font-medium rounded-xl">Text</div>
+                        <div className="bg-white w-full 2xl:py-9 xl:py-8 py-5 2xl:px-0 xl:px-0 px-3 text-center md:text-xl text-sm font-medium rounded-xl">Text</div>
+                        <div className="bg-white w-full 2xl:py-9 xl:py-8 py-5 2xl:px-0 xl:px-0 px-3 text-center md:text-xl text-sm font-medium rounded-xl">Text</div>
+                        <div className="bg-white w-full 2xl:py-9 xl:py-8 py-5 2xl:px-0 xl:px-0 px-3 text-center md:text-xl text-sm font-medium rounded-xl">Text</div>
+                        <div className="bg-white w-full 2xl:py-9 xl:py-8 py-5 2xl:px-0 xl:px-0 px-3 text-center md:text-xl text-sm font-medium rounded-xl">Text</div>
                     </div>
                 </div>
             </div>

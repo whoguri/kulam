ac7a05e42bf6940be4b8c0aa320e40e5bfe5b189
Kulwinder Singh <kulwinder@fkointech.com>
2024-08-30 15:32:15 +0530

    uploag img

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -29,17 +29,21 @@ export default function Hero() {
             <div className="2xl:max-w-7xl xl:max-w-6xl max-w-[90%] mx-auto py-10">
                 <div className="md:flex gap-10 w-full">
                     <div className="md:w-[15%] w-full flex md:flex-col flex-row md:gap-10 gap-3 justify-between md:mb-0 mb-5">
-                        <div className="bg-white w-full 2xl:py-9 xl:py-8 py-5 2xl:px-0 xl:px-0 px-3 text-center paragraph rounded-xl">Deal 1</div>
+                        <Image src="/images/1.jpeg" alt="1" width={225} height={224} className="rounded-xl w-full h-28  object-cover" />
+                        <Image src="/images/2.jpeg" alt="2" width={225} height={224} className="rounded-xl w-full h-28 object-cover" />
+                        <Image src="/images/3.jpeg" alt="3" width={225} height={224} className="rounded-xl w-full h-28 object-cover" />
+                        <Image src="/images/4.jpeg" alt="4" width={225} height={224} className="rounded-xl w-full h-28 object-cover" />
+                        {/* <div className="bg-white w-full 2xl:py-9 xl:py-8 py-5 2xl:px-0 xl:px-0 px-3 text-center paragraph rounded-xl">Deal 1</div>
                         <div className="bg-white w-full 2xl:py-9 xl:py-8 py-5 2xl:px-0 xl:px-0 px-3 text-center paragraph rounded-xl">Deal 2</div>
                         <div className="bg-white w-full 2xl:py-9 xl:py-8 py-5 2xl:px-0 xl:px-0 px-3 text-center paragraph rounded-xl">Deal 3</div>
-                        <div className="bg-white w-full 2xl:py-9 xl:py-8 py-5 2xl:px-0 xl:px-0 px-3 text-center paragraph rounded-xl">Deal 4</div>
+                        <div className="bg-white w-full 2xl:py-9 xl:py-8 py-5 2xl:px-0 xl:px-0 px-3 text-center paragraph rounded-xl">Deal 4</div> */}
                     </div>
                     <div className="md:w-[70%] w-full">
                         <Slider {...settings}>
                             {PROJECTS.map((e, i) => <div key={i}>
                                 <div className="md:!flex justify-between gap-5 md:p-8 p-4 bg-white rounded-xl">
                                     <div className="md:w-[30%] w-full">
-                                        <Image src={e.img} alt="{e.img}" width={225} height={224} className="rounded-xl md:w-[225px] w-full md:h-[225px] h-[200px] object-cover" />
+                                        <Image src={e.img} alt="{e.img}" width={225} height={224} className="rounded-xl md:w-[225px] w-full md:h-[225px] h-[200px] object-contain" />
                                     </div>
                                     <div className="text-end md:w-[70%] w-full md:pt-0 pt-6">
                                         <h2 className="subheading">{e.title}</h2>
@@ -71,10 +75,10 @@ export default function Hero() {
                         </Slider>
                     </div>
                     <div className="md:w-[15%] w-full flex md:flex-col flex-row md:gap-10 gap-3 justify-between md:mt-0 mt-14">
-                        <div className="bg-white w-full 2xl:py-9 xl:py-8 py-5 2xl:px-0 xl:px-0 px-3 text-center paragraph rounded-xl">Text</div>
-                        <div className="bg-white w-full 2xl:py-9 xl:py-8 py-5 2xl:px-0 xl:px-0 px-3 text-center paragraph rounded-xl">Text</div>
-                        <div className="bg-white w-full 2xl:py-9 xl:py-8 py-5 2xl:px-0 xl:px-0 px-3 text-center paragraph rounded-xl">Text</div>
-                        <div className="bg-white w-full 2xl:py-9 xl:py-8 py-5 2xl:px-0 xl:px-0 px-3 text-center paragraph rounded-xl">Text</div>
+                        <Image src="/images/5.jpeg" alt="5" width={225} height={224} className="rounded-xl w-full h-28 object-cover" />
+                        <Image src="/images/6.jpeg" alt="6" width={225} height={224} className="rounded-xl w-full h-28 object-cover" />
+                        <Image src="/images/7.jpeg" alt="7" width={225} height={224} className="rounded-xl w-full h-28 object-cover" />
+                        <Image src="/images/8.jpeg" alt="8" width={225} height={224} className="rounded-xl w-full h-28 object-cover" />
                     </div>
                 </div>
             </div>

94cab2d746d9cb5ea4a62ea76e3e41062e97946a
rajni62 <rajni@fkointech.com>
2024-09-03 15:20:15 +0530

    Deals detail

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Loading({ style }) {
-    return <div style={style} className="w-full flex items-center justify-center h-screen text-center mx-auto">
+    return <div style={style} className="w-full flex items-center justify-center h-[40vh] text-center mx-auto">
         <div className="loadingio-spinner-reload-nq4q5u6dq7r"><div className="ldio-x2uulkbinbj">
             <div><div></div><div></div><div></div></div>
         </div></div>

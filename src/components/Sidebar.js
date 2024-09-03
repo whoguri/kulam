f0395ad54db7b42057090a5da7314c67218f13c2
grfityhero <grfityhero@gmail.com>
2024-09-03 09:33:35 +0300

    fillRule in svgs creates error in dom

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { getError } from "helper";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
@@ -8,35 +8,35 @@ import { useEffect, useRef, useState } from "react";
 import { toast } from "react-toastify";
 
 export default function Sidebar({ open, setOpen }) {
-  const pathname = usePathname()
+  const pathname = usePathname();
   const ref = useRef(null);
-  const { status } = useSession()
-  const [sending, setSending] = useState(false)
+  const { status } = useSession();
+  const [sending, setSending] = useState(false);
 
   const googleLogin = async () => {
     try {
-      setSending(true)
-      const res = await signIn("google", { callbackUrl: "/" })
+      setSending(true);
+      const res = await signIn("google", { callbackUrl: "/" });
       if (res?.status === 200) {
-        window.location.reload()
+        window.location.reload();
       }
     } catch (e) {
-      console.error(e)
-      toast.error(getError(e))
-      setSending(false)
+      console.error(e);
+      toast.error(getError(e));
+      setSending(false);
     }
-  }
+  };
 
   useEffect(() => {
     const checkIfClickedOutside = (e) => {
-      if ((ref.current && (!ref.current.contains(e.target)))) {
-        setOpen(false)
+      if (ref.current && !ref.current.contains(e.target)) {
+        setOpen(false);
       }
     };
     if (typeof window !== "undefined") {
-      document.addEventListener('mousedown', checkIfClickedOutside);
+      document.addEventListener("mousedown", checkIfClickedOutside);
       return () => {
-        document.removeEventListener('mousedown', checkIfClickedOutside);
+        document.removeEventListener("mousedown", checkIfClickedOutside);
       };
     }
   }, []);
@@ -50,19 +50,24 @@ export default function Sidebar({ open, setOpen }) {
     { title: "הגדרות", link: "/profile" },
     { title: "שירותים", link: "/services" },
     { title: "צור קשר", link: "/contact" },
-
-  ]
+  ];
   return (
     <div>
       <div
         ref={ref}
-        className={`md:hidden bg-background inset-y-0 md:w-[300px] w-3/4 fixed z-30 h-screen transition-all duration-300 ${open ? "left-0" : "-left-full"
-          }`}
+        className={`md:hidden bg-background inset-y-0 md:w-[300px] w-3/4 fixed z-30 h-screen transition-all duration-300 ${
+          open ? "left-0" : "-left-full"
+        }`}
       >
         <div className="w-full flex flex-col justify-between mx-auto py-5 px-6 ">
           <div className="flex items-center justify-between w-full pb-3 ">
             <Link href="/">
-              <Image src="/images/logo.jpeg" alt="logo" width={70} height={70} />
+              <Image
+                src="/images/logo.jpeg"
+                alt="logo"
+                width={70}
+                height={70}
+              />
             </Link>
             <div
               className="cursor-pointer text-primary absolute right-6"
@@ -70,11 +75,18 @@ export default function Sidebar({ open, setOpen }) {
                 setOpen(false);
               }}
             >
-              <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"
-              > <path fill="currentColor"
-                fill-rule="evenodd"
-                d="M6.793 6.793a1 1 0 0 1 1.414 0L12 10.586l3.793-3.793a1 1 0 1 1 1.414 1.414L13.414 12l3.793 3.793a1 1 0 0 1-1.414 1.414L12 13.414l-3.793 3.793a1 1 0 0 1-1.414-1.414L10.586 12L6.793 8.207a1 1 0 0 1 0-1.414"
-                clipRule="evenodd"
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="28"
+                height="28"
+                viewBox="0 0 24 24"
+              >
+                {" "}
+                <path
+                  fill="currentColor"
+                  fillRule="evenodd"
+                  d="M6.793 6.793a1 1 0 0 1 1.414 0L12 10.586l3.793-3.793a1 1 0 1 1 1.414 1.414L13.414 12l3.793 3.793a1 1 0 0 1-1.414 1.414L12 13.414l-3.793 3.793a1 1 0 0 1-1.414-1.414L10.586 12L6.793 8.207a1 1 0 0 1 0-1.414"
+                  clipRule="evenodd"
                 />
               </svg>
             </div>
@@ -86,11 +98,19 @@ export default function Sidebar({ open, setOpen }) {
                 return (
                   <Link
                     onClick={() => {
-                      setOpen(false)
+                      setOpen(false);
                     }}
                     key={i}
                     href={e.link || "/"}
-                    className={`${pathname === e.link ? "border-b border-white" : "font-normal"} ${i !== 5 && "w-full"} my-2 hover:font-bold capitalize text-lg leading-8 block text-white whitespace-pre`}>{e.title}
+                    className={`${
+                      pathname === e.link
+                        ? "border-b border-white"
+                        : "font-normal"
+                    } ${
+                      i !== 5 && "w-full"
+                    } my-2 hover:font-bold capitalize text-lg leading-8 block text-white whitespace-pre`}
+                  >
+                    {e.title}
                   </Link>
                 );
               })}

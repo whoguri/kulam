f38c9650a76816c667ab11aed78765712c1f5319
Gurwinder Singh <whogurwinder@gmail.com>
2024-09-06 12:19:52 +0530

    ...

--- src/pages/api/auth/error.js
+++ src/pages/api/auth/error.js
@@ -1,5 +1,4 @@
 export default async function handle(req, res) {
-    const { error } = req.query
-    res.status(200).end(error);
-
+    res.redirect("/error?e=" + req.query?.error || "Unknown")
+    // res.status(200).end(`error: ${JSON.stringify(req.query)}, ${JSON.stringify(req.body)}`);
 }

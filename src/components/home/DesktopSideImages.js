2188f506ffbd907512cdd7d2c3443de61f87deb0
rajni62 <rajni@fkointech.com>
2024-09-23 11:08:15 +0530

    Added text with images

--- src/components/home/DesktopSideImages.js
+++ src/components/home/DesktopSideImages.js
@@ -5,8 +5,13 @@ function DesktopSideImages({ IMAGES = [] }) {
     return (<div className="md:block hidden">
         <div className="flex md:flex-col flex-row md:gap-10 gap-3">
             {IMAGES.map((e, i) => {
-                return <Image key={i} src={`/images/${e}`} alt="1" width={225} height={224} className="rounded-xl md:w-full w-28 h-28 object-cover" />
+                return <div>
+                    <Image key={i} src={`/images/${e}`} alt="1" width={225} height={224} className="rounded-xl md:w-full w-28 h-28 object-cover" />
+
+                    <div className='text-base mt-1 leading-5 font-semibold text-background text-center line-clamp-2'>In publishing and graphic design,Lorem</div>
+                </div>
             })}
+
         </div>
     </div>)
 }
--- src/components/home/MobileImageSlider.js
+++ src/components/home/MobileImageSlider.js
@@ -35,8 +35,13 @@ export default function MobileImageSlider({ IMAGES = [] }) {
         <div className="md:hidden block">
             <div ref={sliderRef} className="slider-to-left flex md:flex-col flex-row md:gap-10 gap-3">
                 {IMAGES.map((e, i) => {
-                    return <Image key={i} src={`/images/${e}`} alt="1" width={225} height={224} className="rounded-xl md:w-full w-28 h-28 object-cover" />
+                    return <div className=''>
+                        <Image key={i} src={`/images/${e}`} alt="1" width={225} height={224} className="rounded-xl md:w-full max-w-28 w-28 h-28 max-h-28 object-cover" />
+                        <div className='md:text-base text-sm mt-1 leading-4 font-semibold text-background text-center line-clamp-2'>In publishing and graphic design, Lorem</div>
+
+                    </div>
                 })}
+
             </div>
         </div>
     );

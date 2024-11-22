51ac51eb693ded062ff413d632df7fd04274c539
Parminder Kaur <parminder@fkointech.com>
2024-11-22 17:40:08 +0530

    cards ui

--- src/components/DealsComponent.js
+++ src/components/DealsComponent.js
@@ -107,8 +107,8 @@ function Item({ isAdmin, isAdvertiser, setSelId, setOpenDeal, e, i }) {
             <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4" width="1em" height="1em" viewBox="0 0 36 36"><path fill="currentColor" d="M33.87 8.32L28 2.42a2.07 2.07 0 0 0-2.92 0L4.27 23.2l-1.9 8.2a2.06 2.06 0 0 0 2 2.5a2 2 0 0 0 .43 0l8.29-1.9l20.78-20.76a2.07 2.07 0 0 0 0-2.92M12.09 30.2l-7.77 1.63l1.77-7.62L21.66 8.7l6 6ZM29 13.25l-6-6l3.48-3.46l5.9 6Z" className="clr-i-outline clr-i-outline-path-1" /><path fill="none" d="M0 0h36v36H0z" /></svg>
         </button>}
         <Link href={`/deals/${e.id}`} >
-            {e.image && <Image src={e.image} width={200} height={200} className="group-hover:scale-125 transition-all w-full h-36 object-cover" placeholder="empty" />}
-            <div className="group-hover:opacity-100 opacity-0 absolute inset-0 bg-black bg-opacity-70 text-white flex justify-center items-center flex-col transition-all duration-300">
+            <Image src={e.image || "/placeholder.webp"} width={200} height={200} className="group-hover:scale-125 transition-all w-full h-36 object-cover object-center" placeholder="empty" />
+            <div className="group-hover:opacity-100 opacity-0 absolute inset-0 bg-background bg-opacity-70 text-white flex justify-center items-center flex-col transition-all duration-300">
                 <h2 className="subheading">₪{e.amount}</h2>
                 <p className="paragraph 2xl:pt-3 xl:pt-2 pt-2">{e.name}</p>
             </div>

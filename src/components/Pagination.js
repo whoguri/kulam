0a5ede4c89689e4fd3e6097c165c4d1fe3475640
Gurwinder Singh <whogurwinder@gmail.com>
2024-12-18 12:32:22 +0530

    pagination ui

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -9,14 +9,14 @@ export default function Pagination(props) {
 
 
     pageCount = parseInt(pageCount)
-    let start = page - 4
+    let start = page - 3
     if (start < 0) {
         start = 0
     }
-    let end = start + 9
+    let end = start + 7
     if (end > pageCount) {
         end = pageCount
-        start = end - 9
+        start = end - 7
         if (start < 0) {
             start = 0
         }
@@ -33,7 +33,7 @@ export default function Pagination(props) {
                     window.scrollTo(0, 0)
                     setPage(page - 1)
                 }
-            }} disabled={page === 0} className="disabled:pointer-events-none disabled:opacity-60 hover:bg-blue-50 hover:text-white cursor-pointer transition duration-300 ease-in-out  md:px-2 md:py-2 px-1 py-1 rounded-s-md flex items-center justify-center text-xs md:text-sm outline-0">
+            }} disabled={page === 0} className="disabled:pointer-events-none disabled:opacity-60 hover:bg-primary hover:text-white cursor-pointer transition duration-300 ease-in-out  md:px-2 md:py-2 px-1 py-1 rounded-s-md flex items-center justify-center text-xs md:text-sm outline-0">
                 {/* <svg className="md:h-5 md:w-5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m14 18l-6-6l6-6l1.4 1.4l-4.6 4.6l4.6 4.6L14 18Z" /></svg> */}
                 <svg className="md:h-5 md:w-5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12.6 12L8 7.4L9.4 6l6 6l-6 6L8 16.6l4.6-4.6Z" /></svg>
 
@@ -47,8 +47,8 @@ export default function Pagination(props) {
                         setPage(e)
                     }
                 }}
-                className={`${page === e ? "bg-primary text-white hover:text-white cursor-default" : "hover:bg-white hover:text-background text-pribg-primary"} cursor-pointer font-bold block lg:px-2 text-center lg:py-2 px-2 py-1 text-xs lg:text-sm outline-0`}>
-                <div className="md:h-5 md:w-5 h-4 w-4">{e + 1}
+                className={`${page === e ? "bg-primary text-white hover:text-white cursor-default" : "hover:bg-white hover:text-background text-pribg-primary"} cursor-pointer font-bold block lg:px-2 text-center lg:py-2 px-1 py-1 text-xs lg:text-sm outline-0`}>
+                <div className="md:h-5 md:w-5 h-4 w-auto">{e + 1}
                 </div>
             </div>
             )}
@@ -58,7 +58,7 @@ export default function Pagination(props) {
                     window.scrollTo(0, 0)
                     setPage(page + 1)
                 }
-            }} disabled={!(page < parseInt(pageCount))} className="disabled:opacity-60 disabled:pointer-events-none hover:bg-primary hover:text-white cursor-pointer font-medium block lg:px-2 lg:py-2 px-2 py-1 rounded-e-lg text-xs lg:text-sm outline-0">
+            }} disabled={!(page < parseInt(pageCount))} className="disabled:opacity-60 disabled:pointer-events-none hover:bg-primary hover:text-white cursor-pointer font-medium block lg:px-2 lg:py-2 px-1 py-1 rounded-e-lg text-xs lg:text-sm outline-0">
                 <span className="sr-only">Last</span>
                 {/* <svg className="md:h-5 md:w-5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12.6 12L8 7.4L9.4 6l6 6l-6 6L8 16.6l4.6-4.6Z" /></svg> */}
 
@@ -71,7 +71,7 @@ export default function Pagination(props) {
                     onChange={(e) => {
                         setLimit(parseInt(e.target.value))
                     }}
-                    className="text-pribg-primary font-light w-auto block lg:px-2 lg:py-2 px-2 py-1 border border-[#080A24] rounded-lg text-xs lg:text-sm  outline-0">
+                    className="text-pribg-primary font-light w-auto block lg:px-2 lg:py-2 px-1 py-1 border border-[#080A24] rounded-lg text-xs lg:text-sm  outline-0">
                     <option value={10}>10</option>
                     <option value={20}>20</option>
                     <option value={30}>30</option>
